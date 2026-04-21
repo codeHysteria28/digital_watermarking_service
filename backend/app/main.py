fb3727d288c5a742b862dfecfed64a245e07a887
Damian Przysiwek <123418197+itsdamo@users.noreply.github.com>
2026-04-21 23:37:27 +0100

    Adding CORS security allowlisting for backend-frontend integration.

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,6 +5,8 @@ from app.routes.image_route import router as images_router
 from app.routes.verification_route import router as verification_router
 from app.models.base import Base
 from app.core.database import engine
+from fastapi.middleware.cors import CORSMiddleware
+import os
 
 Base.metadata.create_all(bind=engine)
 
@@ -13,6 +15,19 @@ app = FastAPI(
     description="Backend service for managing users and digital watermarking operations.",
     version="1.0.0"
 )
+
+# CORS Issues, sicne frontend and backend will be deployed on different URLs we need to setup CORS origin allowlist
+# By default it will use wildcard CORS if nothing is specified in the ALLOWED_ORIGINS env variable.
+allowed_origins = os.getenv("ALLOWED_ORIGINS", "*").split(",")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=False,  # must be False when using wildcard origin
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(users_router)
 app.include_router(images_router)
 app.include_router(verification_router)
